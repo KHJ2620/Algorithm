63ee28d0286fa2529dd999d8a20d80f1bce989d0
김효정 <136438704+KHJ2620@users.noreply.github.com>
2024-05-16 20:39:29 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int multi = 1;
+        int plus = 0;
+        for(int i=0; i<num_list.length; i++){
+            multi *= num_list[i];
+            plus += num_list[i];            
+        }
+        if(multi < plus*plus) {
+                answer = 1;
+            }
+        return answer;
+    }
+}
